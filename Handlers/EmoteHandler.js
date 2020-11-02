b4b30af42ce32db90b15a9f253c050090fd86882
dew <70388244+dewbyte@users.noreply.github.com>
2020-11-02 15:06:37 -0500

    changed syntax to always get the longest emote

--- Handlers/EmoteHandler.js
+++ Handlers/EmoteHandler.js
@@ -7,12 +7,22 @@ const bot = new Discord.Client();
 function emoteHandler(stats, msg, type) {
     var settings = JSON.parse(fs.readFileSync("settings.json"));
     var sendback = "";
+    var matches = [];
+    var highestlength = [0, ""]
 
     if (type != "dm") {
         for (em in settings[stats[2]].emotes) {
             if (msg.content.includes(settings[stats[2]].emotes[em].code)) {
-                sendback = "https://static-cdn.jtvnw.net/emoticons/v1/" + settings[stats[2]].emotes[em].id.toString() + "/" + settings[stats[2]].defaultsize;
+                matches.push(settings[stats[2]].emotes[em].code);
             }
+        }
+        if (matches.length != 0) {
+            for(item in matches) {
+                if (item.length > highestlength[0]) {
+                    var highestlength = [item.length, item]
+                }
+            }
+            var sendback = "https://static-cdn.jtvnw.net/emoticons/v1/" + highestlength[1].id.toString() + "/" + settings[stats[2]].defaultsize;
         }
     } else {
         var sendback = "you need to use \"getemote\" in direct messages, there is no emote detection in direct messages.";
